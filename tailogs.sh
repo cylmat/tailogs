1f6b0fc9557f817598ce7f7f4f5752534de4954e
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-04-21 21:50:30 +0200

    Update tailogs.sh

--- tailogs.sh
+++ tailogs.sh
@@ -5,31 +5,33 @@
 ###################
 
 _tailogs_display_usage() {
-    echo -e "Usage: $0 [arguments] \n
-Arguments: \n
+    echo -e "\nUsage: $0 [arguments]
+Arguments:
  -g, --logfile=<file.log> \t Log filename
  -l, --limit \t\t\t Limit to terminal screensize 
-in progress..."
+in progress...\n"
     exit 0
 }
 
 _tailogs_getoptions() {
-    # --(l)imit-terminal-size
-    for i in "$@"; do
-        case $i in
+    # @todo --(l)imit-terminal-size
+    ARGS=()
+    for param in "$@"; do
+        case $param in
             --grep=*)
-                GREP_PATTERN="${i#*=}"
+                GREP_PATTERN="${param#*=}"
             ;;
             --logfile=*)
-                FILEPATH="${i#*=}"
+                FILEPATH="${param#*=}"
             ;;
             --help)
                 _tailogs_display_usage
                 exit 0
             ;;
             *)
-                _tailogs_display_usage
-                exit 0
+                # don't use set -- "$@" "$param" ;;
+                # cause we keep every others params
+                ARGS+=($param)
             ;;
         esac
     done
@@ -39,9 +41,9 @@ _tailogs_getoptions() {
 
 _dev_config() {
     FILEPATH=/var/log/apt/history.log
-    PATTERN="txton-txtwo: date  time"
-    NEW_PATTERN="txton-txtwo: time date?"
-    APACHE=("$PATTERN" "$NEW_PATTERN")
+    ACTUAL_PATTERN="txton-txtwo: date  time"
+    NEW_PATTERN="33txton0-32txtwo0: 33time0 31date0?"
+    APACHE=("$ACTUAL_PATTERN" "$NEW_PATTERN")
     TYPE='APACHE'
 }
 
@@ -79,10 +81,28 @@ _tailogs_getindex_from_item() {
     done
 }
 
-# use NEW_PATTERN, PATTERN_ITEMS
+# use NEW_PATTERN
+# return COLORED_NEWPATTERN
+_tailogs_color_newpattern() {
+    COLOR_PATTERN="\\e[\1m" #\033[0;32m | \e[32m
+    RESET_PATTERN="\\e[0m"  #\033[0m \e[0m
+    local _31="$(echo -e "\033[0;31m")"
+    local _32="$(echo -e "\033[0;32m")"
+    local _33="$(echo -e "\033[0;33m")"
+    local _34="$(echo -e "\033[0;34m")"
+    local _0="$(echo -e "\033[0m")"
+
+    COLORED_NEWPATTERN=$(sed -E "s/0/$_0/g" <<< "$NEW_PATTERN")
+    COLORED_NEWPATTERN=$(sed -E "s/(31)/$_31/g" <<< "$COLORED_NEWPATTERN")
+    COLORED_NEWPATTERN=$(sed -E "s/(32)/$_32/g" <<< "$COLORED_NEWPATTERN")
+    COLORED_NEWPATTERN=$(sed -E "s/(33)/$_33/g" <<< "$COLORED_NEWPATTERN")
+    COLORED_NEWPATTERN=$(sed -E "s/(34)/$_34/g" <<< "$COLORED_NEWPATTERN")
+}
+
+# use COLORED_NEWPATTERN, PATTERN_ITEMS
 # return INDEXED_NEWPATTERN
 _tailogs_replace_newpattern_by_indexes() {
-    INDEXED_NEWPATTERN=$NEW_PATTERN
+    INDEXED_NEWPATTERN=$COLORED_NEWPATTERN
     for i in "${!PATTERN_ITEMS[@]}"; do
         local item="${PATTERN_ITEMS[$i]}"
         _tailogs_getindex_from_item $item INDEX
@@ -93,6 +113,7 @@ _tailogs_replace_newpattern_by_indexes() {
 # use PATTERN, NEW_PATTERN
 _tailogs_process_pattern() {
     _tailogs_pattern_to_stars #STAR_PATTERN=(.*)\-(.*)\: (.*) (.*)
+    _tailogs_color_newpattern #COLORED_NEWPATTERN
     _tailogs_set_pattern_items #PATTERN_ITEMS=txton txtwo date time
     _tailogs_replace_newpattern_by_indexes #INDEXED_NEWPATTERN=\1-\2: \4 \3?
 }
@@ -110,11 +131,18 @@ _tailogs_set_size_term() {
 # use FILEPATH, SIZE_TERM, GREP_PATTERN
 # use STAR_PATTERN, INDEXED_NEWPATTERN
 _tailogs_run_tail() {
-    tail $FILEPATH | \
+    # echo "tail ${ARGS[@]} $FILEPATH | \
+    # cut -c -$SIZE_TERM | \
+    # grep --color $GREP_PATTERN | \
+    # sed -E 's/$STAR_PATTERN/$INDEXED_NEWPATTERN/'"; exit
+    
+    
+    tail "${ARGS[@]}" $FILEPATH | \
     cut -c -$SIZE_TERM | \
     grep --color "$GREP_PATTERN" | \
-    sed -E "s/$STAR_PATTERN/$INDEXED_NEWPATTERN/"
-    # ccze -A
+    sed --unbuffered -E "s/$STAR_PATTERN/$INDEXED_NEWPATTERN/"
+    #sed --unbuffered -E -e "s/.*/$_33&dfgqg$_0/g"
+    # grc tail, tail | ccze -A
 }
 
 ########
@@ -129,44 +157,13 @@ main() {
     # process
     _tailogs_get_patterns_from_config
     _tailogs_process_pattern #STAR_PATTERN, INDEXED_NEWPATTERN
+
     # options
     _tailogs_getoptions "$@"
     _tailogs_set_size_term #SIZE_TERM
 
     # RUN #
-    _tailogs_run_tail
+    _tailogs_run_tail 
 }
+
 main "$@"
-
-
-#@todo
-# _tailogs_longparams() {
-#     for param in "$@"; do
-#         shift
-#         case "$param" in
-#             "--file") set -- "$@" "-f" ;;
-#             "--help") set -- "$@" "-h" ;;
-#             *) set -- "$@" "$param" ;;
-#         esac
-#     done
-# }
-
-#@todo
-# _tailogs_shortparams() {
-#     OPTIND=1
-#     while getopts "fh" opt; do
-#         case "$opt" in
-#             "h")
-#                 _tailogs_display_usage
-#                 exit 0
-#                 ;;
-#             "f") file=1 ;;
-#             "?")
-#                 _tailogs_display_usage >&2
-#                 exit 1
-#                 ;;
-#         esac
-#     done
-
-#     shift $((OPTIND - 1))
-# }
