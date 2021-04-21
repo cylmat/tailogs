0b573d599924c6cf5b4c6cf2e74031d5a636783b
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-04-21 18:13:05 +0200

    Update tailogs.sh

--- tailogs.sh
+++ tailogs.sh
@@ -1,25 +1,83 @@
 #!/usr/bin/env bash
 
-# var/logs/dev.log
+###################
+# Define commands #
+###################
+
+_tailogs_getoptions() {
+    # --(l)imit-terminal-size
+    for i in "$@"; do
+        case $i in
+            --grep=*)
+                GREP_PATTERN="${i#*=}"
+            ;;
+            --logfile=*)
+                FILEPATH="${i#*=}"
+            ;;
+            --help)
+                _tailogs_display_usage
+                exit 0
+            ;;
+            *)
+                _tailogs_display_usage
+                exit 0
+            ;;
+        esac
+    done
+}
+
+_tailogs_display_usage() {
+    echo -e "Usage: $0 [arguments] \n
+Arguments: \n
+ -g, --logfile=<file.log> \t Log filename
+ -l, --limit \t\t\t Limit to terminal screensize 
+in progress..."
+    exit 0
+}
+
+_tailogs_getindex() {
+    for i in "${!ARRAY[@]}"; do
+        if [[ $1 == "${ARRAY[$i]}" ]]; then RES=$(($i+1)); return; fi
+    done
+}
+
+_tailogs_replace_pattern_by_index() {
+    for i in "${!ARRAY[@]}"; do
+        local name="${ARRAY[$i]}"
+        _tailogs_getindex $name RES
+        index=$RES
+        NEW_PAT=$(sed "s/$name/\\\\$index/g" <<< $NEW_PAT)
+    done
+}
+
+_tailogs_run_tail() {
+    tail $FILEPATH | \
+    cut -c -$SIZE_TERM | \
+    grep --color "$GREP_PATTERN" | \
+    sed -E "s/${TYPE[0]}/${TYPE[1]}/"
+    # ccze -A
+}
+
+_dev_config() {
+    PATTERN="txton-txtwo: date  time"
+    NEW_PATTERN="txton-txtwo: time date?"
+}
+
+########
+# MAIN #
+########
 
 main() {
 
 RES=''
 
-display_usage() {
-    echo -e "Usage: tailogs [OPTIONS]\n
- -f \t\t\t Follow
-    --file=<file.log> \t Filename"
-}
+_dev_config
 
 #PATTERN="[date time] name.type: message"
 #NEW_PATTERN="type:name date"
 
 #APACHE=("(snc_redis).(DEBUG)" "\2.\1")
 #PATTERN="[date time] name.type: message"
-PATTERN="ni-no: datetruc  timetruc"
-NEW_PATTERN="ni-no: timetruc datetruc?"
-#\4:\3 \1
 
 
 M_PATTERN=$(sed -E 's/([^a-z ])/\\\1/g' <<< "$PATTERN")
@@ -27,28 +85,10 @@ M_PATTERN=$(sed -E 's/(\w+)/(.*)/g' <<< "$M_PATTERN")
 # date time name type message
 ARRAY=($(sed -E 's/[^a-z]+/ /g' <<< "$PATTERN"))
 
-
-getindex() {
-    for i in "${!ARRAY[@]}"; do
-        if [[ $1 == "${ARRAY[$i]}" ]]; then RES=$(($i+1)); return; fi
-    done
-}
-
-#getindex 'name' RES
-
 NEW_PAT=$NEW_PATTERN
 
 
-replace_pattern_by_index() {
-    for i in "${!ARRAY[@]}"; do
-        local name="${ARRAY[$i]}"
-        getindex $name RES
-        index=$RES
-        NEW_PAT=$(sed "s/$name/\\\\$index/g" <<< $NEW_PAT)
-    done
-}
-
-replace_pattern_by_index
+_tailogs_replace_pattern_by_index
 #echo $NEW_PAT
 
 
@@ -56,31 +96,7 @@ replace_pattern_by_index
 APACHE=("$M_PATTERN" "$NEW_PAT")
 #echo $M_PATTERN;
 
-getoptions() {
-    # --(l)imit-terminal-size
-    for i in "$@"; do
-        case $i in
-            -f)
-                FOLLOW=1
-            ;;
-            --grep=*)
-                GREP_PATTERN="${i#*=}"
-            ;;
-            --file=*)
-                FILEPATH="${i#*=}"
-            ;;
-            --help)
-                display_usage
-                exit 0
-            ;;
-            *)
-                exit 0
-            ;;
-        esac
-    done
-}
-
-getoptions "$@"
+_tailogs_getoptions "$@"
 TYPE=("${APACHE[@]}")
 
 export SIZE_TERM=$(stty size | cut -d ' ' -f 2)
@@ -89,10 +105,41 @@ SIZE_TERM=$(($SIZE_TERM-1))
 FILEPATH=/var/log/apt/history.log
 
 # RUN
-tail $FILEPATH | cut -c -$SIZE_TERM | grep --color "$GREP_PATTERN" | \
-    sed -E "s/${TYPE[0]}/${TYPE[1]}/"
-# ccze -A
+_tailogs_run_tail
 
 }
 
 main "$@"
+
+
+#@todo
+# _tailogs_longparams() {
+#     for param in "$@"; do
+#         shift
+#         case "$param" in
+#             "--file") set -- "$@" "-f" ;;
+#             "--help") set -- "$@" "-h" ;;
+#             *) set -- "$@" "$param" ;;
+#         esac
+#     done
+# }
+
+#@todo
+# _tailogs_shortparams() {
+#     OPTIND=1
+#     while getopts "fh" opt; do
+#         case "$opt" in
+#             "h")
+#                 _tailogs_display_usage
+#                 exit 0
+#                 ;;
+#             "f") file=1 ;;
+#             "?")
+#                 _tailogs_display_usage >&2
+#                 exit 1
+#                 ;;
+#         esac
+#     done
+
+#     shift $((OPTIND - 1))
+# }
