37607c050da34750730c2a474a530491851d3e63
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-04-27 21:40:42 +0200

    Create tailogs.pl

---
+++ tailogs.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+# https://www.geeksforgeeks.org/perl-warnings-and-how-to-handle-them/
+
+use strict;
+use warnings;
+
+###################
+# Define commands #
+###################
+
+sub dev_config()
+{
+
+}
