7cc6da80ea129f79035f79cb668deda06c79d4ac
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-04-28 12:20:33 +0200

    Update tailogs.pl

--- tailogs.pl
+++ tailogs.pl
@@ -8,19 +8,61 @@ use warnings;
 # apt-get install -y locales locales-all
 # echo -e 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8' > /etc/default/locale
 # print "@{[ %hash ]}";
+# foreach my $k (sort keys %config) { print "$k => $config{$k}\n"; }
+# while ( ($k,$v) = each %hash ) { print "$k => $v\n"; }
+#
+#
+# my %HoA = ( 
+#   flintstones => [
+#     "fred", 
+#     "barney"
+#   ], 
+#
+# foreach $number ( @numbers ) {
+#   for my $family ( sort keys %HoA ) {
+#   print "$family: "; 
+#   while (my ($index, $elem) = each @{$HoA{$family}}) {
+#     print "$index $elem ";
+#   }
+#   print "\n"; 
+#   } 
 
 ###################
 # Define commands #
 ###################
 
+use constant TAIL => "/usr/bin/tail";
+
+# for my $family ( sort keys %HoA ) {
+#   print "$family: "; 
+#   while (my ($index, $elem) = each @{$HoA{$family}}) {
+#     print "$index $elem ";
+#   }
+#   print "\n"; 
+# }
+
 sub get_config {
-  my %config;
+  my %config = ();
   my $filename = "/var/log/apt/history.log";
 
   $config{'filename'} = $filename;
-  $config{'pattern'} = "log1 log2";
-  $config{'new_pattern'} = "alpha5";
-   
+  my @actual_patterns = [
+    "command: msg",
+    "install: msg2",
+    "txtdate: date time"
+  ];
+  my @new_pattern = [
+    "msg-command",
+    "msg2 local",
+    "date date time time"
+  ];
+  @config{'actual_pattern'} = @actual_patterns;
+  @config{'new_pattern'} = @new_pattern;
+
+  while (my ($index, $elem) = each @{$config{'actual_pattern'}}) {
+    print "$elem\n";
+  }
+  exit;
   return %config;
 }
 
@@ -32,26 +74,21 @@ sub get_config {
 # MAIN #
 ########
 
-# param %config
 sub run_tail(%) {
-  my %config = %_[0];
-  #print "@_";
+  my %config = @_;
   
-  my $tail_cmd = "/usr/bin/tail";
   my @args = ("-n 3");
-  push(@args, "");
-  my $filename='';
+  push(@args, $config{'filename'});
 
-  my $tailcmd_args = "$tail_cmd @args $filename";
+  my $tail_args = TAIL . " @args";
+  #print $tail_args; exit; #DEBUG
 
-  open my $pipe, "-|", $tailcmd_args or die "could not start tail on SampleLog.log: $!";
-  print while <$pipe>;
+  open my $tail_pipe, "-|", $tail_args or die "Error - Could not start tail on $config{'filename'}: $!";
+  print while <$tail_pipe>;
 }
 
 sub main {
   my %config = get_config();
-  print keys %config;
-  exit;
   run_tail(%config);
 }
 
