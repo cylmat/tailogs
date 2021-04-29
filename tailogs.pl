a8dde34dfc5bd15c903167c20865cd3426c09037
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-04-29 21:07:22 +0200

    Update tailogs.pl

--- tailogs.pl
+++ tailogs.pl
@@ -5,36 +5,16 @@
 use strict;
 use warnings;
 
-# apt-get install -y locales locales-all
+# apt-get update && apt-get install -y locales locales-all
 # echo -e 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8' > /etc/default/locale
 # print "@{[ %hash ]}";
 # foreach my $k (sort keys %config) { print "$k => $config{$k}\n"; }
 # while ( ($k,$v) = each %hash ) { print "$k => $v\n"; }
-#
-  # "${config{apache}{filename}}";
-  # "${config{apache}{actual_patterns}[1]}";
-#
-# my %HoA = ( 
-#   flintstones => [
-#     "fred", 
-#     "barney"
-#   ], 
-#
-# foreach $number ( @numbers ) {
-#   for my $family ( sort keys %HoA ) {
-#   print "$family: "; 
-#   while (my ($index, $elem) = each @{$HoA{$family}}) {
-#     print "$index $elem ";
-#   }
-#   print "\n"; 
-#   } 
+# "${config{apache}{actual_patterns}[1]}";
 
 ##########
 # GLOBAL #
 ##########
-
-use constant REGEX_CHARS => "[a-z ]";
-use constant REGEX_NOCHARS => "[^a-z ]";
 
 sub replace($$$) {
   my $pattern = $_[0];
@@ -66,6 +46,14 @@ sub get_index($@) { # search_value, @array
   return $index; # beta [alpha, beta] => 1
 }
 
+sub dd($) {
+    print $_[0]; exit;
+}
+
+sub debug($) {
+    #print $_[0]."\n";
+}
+
 ###################
 # Define commands #
 ###################
@@ -75,7 +63,7 @@ sub get_index($@) { # search_value, @array
 #   while (my ($index, $elem) = each @{$HoA{$family}}) {
 #     print "$index $elem ";
 #   }
-#   print "\n"; 
+#   print "\n";
 # }
 
 sub get_config {
@@ -85,12 +73,12 @@ sub get_config {
     "filename" => "/var/log/apt/history.log",
     #"filename" => "/var/log/commerce.log",
     "actual_patterns" => [
-      "command: msg",
+      "Commandline: <command> -y <msg>",
       "install: msg",
       "txtdate: date time"
     ],
     "new_patterns" => [
-      "31msg0 <=> 32command0",
+      "Commandline: 31<msg>0 <=> 32<command>0",
       "msgtwo local",
       "date date time&time"
     ]
@@ -102,23 +90,36 @@ sub get_config {
 ###########
 # PROCESS #
 ###########
+
+=begin
+SLASH command\: msg
+STAR (.*)\: (.*)
+ITEMS command msg
+ITEMSLIST command msg
+NEW 31msg0 <=> 32command0
+COLORNEW msg <=> command
+NEW_BY_ITEMS \2 <=> \1
+=cut
+
 sub slash_nochars_pattern($) {
   my $pattern = "@_";
-  my $slashed_pattern = replace($pattern, "(".REGEX_NOCHARS.")", '\\\\$1');
-  return $slashed_pattern; # alpha:msg => alpha\:msg
+  my $slashed_pattern = replace($pattern, "([^a-z<> ])", '\\\\$1');
+  debug("SLASH $slashed_pattern");
+  return $slashed_pattern; # <alpha>: <msg> => <alpha>\: <msg>
 }
 
 sub pattern_to_stars($) {
   my $slashed_pattern = "@_";
-  my $star_pattern = replace($slashed_pattern, "([a-z]+)", "(.*)");
-  return $star_pattern; # alpha msg => (.*) (.*)
+  my $star_pattern = replace($slashed_pattern, "(<[a-z]+>)", "(.*)");
+  debug("STAR $star_pattern");
+  return $star_pattern; # <alpha> <msg> => (.*) (.*)
 }
 
 sub pattern_to_items($) {
   my $pattern = "@_";
-  $pattern = replace($pattern, "[ ]+", " "); # trim multiple spaces
-  my $only_items = replace($pattern, "[^a-z ]",'');
-  my @items_list = split_spaces($only_items);
+  $pattern = replace($pattern, "\\s+", " "); # Trim multiple spaces
+  my @items_list = ( $pattern =~ /(<[a-z]+>)/g );
+  debug("ITEMSLIST @items_list");
   return @items_list;
 }
 
@@ -138,19 +139,21 @@ sub process_patterns($$) {
   my $pattern = "$_[0]";
   my $new_pattern = "$_[1]";
 
-  my $slashed_pattern = slash_nochars_pattern($pattern); # alpha\: beta
-  my $star_pattern = pattern_to_stars($slashed_pattern); # (.*)\: (.*)
+  #my $slashed_pattern = slash_nochars_pattern($pattern); # alpha\: beta
+  my $star_pattern = pattern_to_stars($pattern); # (.*)\: (.*)
   my @actual_items = pattern_to_items($pattern); # [alpha, beta]
   
   # Colors
+  debug("NEW $new_pattern");
   $new_pattern = number_to_color($new_pattern);
+  debug("COLORNEW $new_pattern");
 
   my $new_pattern_by_items = "$new_pattern";
   for my $item_name (@actual_items) { # alpha beta gamma => 0 1 2
     my $index = get_index($item_name, \@actual_items)+1;
     $new_pattern_by_items = replace($new_pattern_by_items, "($item_name)", "\\\\$index"); 
   }
-
+  debug("NEW_BY_ITEMS $new_pattern_by_items");
   return $new_pattern_by_items;
 }
 
@@ -188,7 +191,7 @@ sub run_tail(%) {
   # Final command #
   push(@pipes, PIPE."$grep".PIPE."$sed");
   my $command = TAIL."@args"."@pipes";
-  #print $command; exit; #DEBUG
+  debug($command);
 
   # RUN TAIL #
   open my $tail_pipe, "-|", $command or die "Error - Could not start tail on $params{'filename'}: $!";
@@ -206,8 +209,8 @@ sub main {
   my $new_pattern = "${new_patterns[0]}";
 
   # Process
-  my $slashed_pattern = slash_nochars_pattern($pattern); # alpha\: beta
-  my $star_pattern = pattern_to_stars($slashed_pattern); # (.*)\: (.*)
+  #my $slashed_pattern = slash_nochars_pattern($pattern); # alpha\: beta
+  my $star_pattern = pattern_to_stars($pattern); # (.*)\: (.*)
 
   my $new_one = process_patterns($pattern, $new_pattern);
 
